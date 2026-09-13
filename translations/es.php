7a09e75262c95c92f0746e82c32b87a5f5621a70
caglarutkuguler <caglar.guler@atilim.edu.tr>
2026-09-13 14:18:41 +0300

    Add review nudge translations for multiple languages

--- translations/es.php
+++ translations/es.php
@@ -241,3 +241,7 @@ $_MODULE['<{priceandorder}prestashop>quoteform_f11b368cddfe37c47af9b9d91c6ba4f0'
 $_MODULE['<{priceandorder}prestashop>quoteform_f3533a2e4c824994e48a0ef4414d8e90'] = 'Cantidad necesaria';
 $_MODULE['<{priceandorder}prestashop>quoteform_fa2ead697d9998cbc65c81384e6533d5'] = 'Política de privacidad';
 $_MODULE['<{priceandorder}prestashop>quoteform_fdbf7ea71413939836ccc4d8f21240e9'] = 'Destino de envío';
+$_MODULE['<{priceandorder}prestashop>megventurereviewnudge_c980491d2b999421c4aa1ef4449323e1'] = '¿Satisfecho con este módulo? Una breve reseña ayuda a otros comerciantes a encontrarlo.';
+$_MODULE['<{priceandorder}prestashop>megventurereviewnudge_6ecbf3c5ab7a06f08c526f02bce2b2d6'] = 'Dejar una reseña';
+$_MODULE['<{priceandorder}prestashop>megventurereviewnudge_8ad4303b83a62fb6ca3b025bad5bc114'] = 'No, gracias';
+$_MODULE['<{priceandorder}prestashop>megventurereviewnudge_4d170dcb0d03a0efa011c9193219bb68'] = 'De acuerdo, no volveremos a preguntar.';
--- translations/fr.php
+++ translations/fr.php
@@ -241,3 +241,7 @@ $_MODULE['<{priceandorder}prestashop>quoteform_f11b368cddfe37c47af9b9d91c6ba4f0'
 $_MODULE['<{priceandorder}prestashop>quoteform_f3533a2e4c824994e48a0ef4414d8e90'] = 'Quantité souhaitée';
 $_MODULE['<{priceandorder}prestashop>quoteform_fa2ead697d9998cbc65c81384e6533d5'] = 'Politique de confidentialité';
 $_MODULE['<{priceandorder}prestashop>quoteform_fdbf7ea71413939836ccc4d8f21240e9'] = 'Destination de livraison';
+$_MODULE['<{priceandorder}prestashop>megventurereviewnudge_c980491d2b999421c4aa1ef4449323e1'] = 'Satisfait de ce module ? Un court avis aide les autres marchands à le trouver.';
+$_MODULE['<{priceandorder}prestashop>megventurereviewnudge_6ecbf3c5ab7a06f08c526f02bce2b2d6'] = 'Laisser un avis';
+$_MODULE['<{priceandorder}prestashop>megventurereviewnudge_8ad4303b83a62fb6ca3b025bad5bc114'] = 'Non merci';
+$_MODULE['<{priceandorder}prestashop>megventurereviewnudge_4d170dcb0d03a0efa011c9193219bb68'] = 'Entendu, nous ne le redemanderons pas.';
